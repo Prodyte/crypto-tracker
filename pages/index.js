ed0dfc8c897c86ae63d53362207a683b4f47812a
prodyte <prodyte@pop-os.localdomain>
2021-08-06 20:13:15 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from "next/head";
-import SearchBar from "../components/SearchBar";
-import CoinsList from "../components/CoinsList";
-import Image from "next/image";
-import { useState } from "react";
-import logo from "../images/logo.png";
+import Head from 'next/head';
+import SearchBar from '../components/SearchBar';
+import CoinsList from '../components/CoinsList';
+import Image from 'next/image';
+import { useState } from 'react';
+import logo from '../images/logo.png';
 
 export default function Home({ filteredCoins }) {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
   const allCoins = filteredCoins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase())
   );
@@ -17,7 +17,7 @@ export default function Home({ filteredCoins }) {
     console.log(e.target.value.toLowerCase());
   };
   return (
-    <div className="bg-gradient-to-r from-primary via-secondary to-danger text-white">
+    <div className="bg-gradient-to-r from-primary via-secondary to-danger text-white h-full w-full">
       <Head>
         <title>Crypto Tracker API</title>
       </Head>
@@ -39,7 +39,7 @@ export default function Home({ filteredCoins }) {
 
 export const getServerSideProps = async () => {
   const res = await fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false'
   );
   const filteredCoins = await res.json();
   return {
